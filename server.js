5e93d146448ac48b7388301c68d33ab5496a6174
Lolit <lolitmohan090@gmail.com>
2023-05-11 23:28:23 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Creating a empty books array for
+let books = [];
+
+//Book route - will Return a JSON array of the Book
+app.get('/books', function (req, res) {
+    res.json(books)
+})
+
+// Add a book
+app.post('/books', (req, res) => {
+    const {title, author, publishDate} = req.body;
+
+    // Create Book ID
+    const id = Date.now().toString();
+
+    // Create new Book Object
+    const book = {
+        id,
+        title,
+        author,
+        publishDate
+    }
+    // Require Title and author
+    if (!title || !author) {
+        return res.status(400).json({ message: 'Title and author are required' });
+    }
+    // add books to the array
+    books.push(book);
+
+    // Send new book details;
+    res.json(book)
+})
+
+// Delete book
+app.delete('/delete', (req, res) => {
+    const id = req.query.id;
+    const bookIndex = books.findIndex(book => book.id === id);
+    if (bookIndex === -1) {
+        return res.status(404).json({ message: 'Book not found' });
+    }
+    const deletedBook = books.splice(bookIndex, 1);
+    res.json({ message: 'Book deleted successfully' });
+})
+
+
+// route
+app.get('/', (req, res )=> {
+    res.sendFile(`${__dirname}/index.html`, )
+})
+
+
+// app Run
+app.listen(8000, function () {
+    console.log("Server Run Success")
+})
